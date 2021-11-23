b877a50b5f9fb71c94a114e3c3b2291aefe5602c
Storm developer <nspm.tests.nora.swapper@gmail.com>
2021-11-23 21:02:57 +0100

    Projektdateien hinzufügen.

---
+++ DynamicSwapper/lib/Core/HexMemory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicSwapper.lib.Core
+{
+    public class HexMemory
+    {
+        public void WriteHex(string hex, string file, int offset)
+        {
+            // write hex in memory
+            BinaryWriter mem = new BinaryWriter((Stream)File.Open(file, FileMode.Open, FileAccess.ReadWrite));
+            mem.BaseStream.Seek(offset, SeekOrigin.Begin);
+            mem.Write(HexToByteArray(hex));
+            mem.Close();
+            // Give the memory away that was used
+            mem.Dispose();
+        }
+        public static byte[] HexToByteArray(string input)
+        {
+            if (input.Contains(" "))
+            {
+                input = input.Replace(" ", "");
+            }
+            if (input.Length % 2 != 0)
+            {
+                throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "ERROR", input));
+            }
+            byte[] bytearray = new byte[input.Length / 2];
+            for (int i = 0; i < bytearray.Length; i++)
+            {
+                string str = input.Substring(i * 2, 2);
+                bytearray[i] = byte.Parse(str, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+            }
+            return bytearray;
+        }
+    }
+}
---
+++ DynamicSwapper/lib/Program.cs
@@ -0,0 +1,49 @@
+﻿using DynamicSwapper.lib.Core;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace DynamicSwapper
+{
+    internal class Program
+    {
+        static HexMemory memory = new HexMemory();
+        static void Main(string[] args)
+        {
+            Console.WriteLine($@"
+   _____ _                       _       _____                              _           _    _____                                    
+  / ____| |                     ( )     |  __ \                            (_)         | |  / ____|                                   
+ | (___ | |_ ___  _ __ _ __ ___ |/ ___  | |  | |_   _ _ __   __ _ _ __ ___  _  ___ __ _| | | (_____      ____ _ _ __  _ __   ___ _ __ 
+  \___ \| __/ _ \| '__| '_ ` _ \  / __| | |  | | | | | '_ \ / _` | '_ ` _ \| |/ __/ _` | |  \___ \ \ /\ / / _` | '_ \| '_ \ / _ \ '__|
+  ____) | || (_) | |  | | | | | | \__ \ | |__| | |_| | | | | (_| | | | | | | | (_| (_| | |  ____) \ V  V / (_| | |_) | |_) |  __/ |   
+ |_____/ \__\___/|_|  |_| |_| |_| |___/ |_____/ \__, |_| |_|\__,_|_| |_| |_|_|\___\__,_|_| |_____/ \_/\_/ \__,_| .__/| .__/ \___|_|   
+                                                 __/ |                                                         | |   | |              
+                                                |___/                                                          |_|   |_|              
+");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("All swaps will be written in memory now, please be patient.");
+
+            foreach (var json in Directory.GetFiles(Environment.CurrentDirectory + "\\Swaps"))
+            {
+                memory.WriteHex(GetResult(json, "hexCode1"), GetResult(json, "file1"), int.Parse(GetResult(json, "offset")));
+                memory.WriteHex(GetResult(json, "hexCode2"), GetResult(json, "file2"), int.Parse(GetResult(json, "offset2")));
+                memory.WriteHex(GetResult(json, "hexCode3"), GetResult(json, "file3"), int.Parse(GetResult(json, "offset3")));
+                Console.ForegroundColor = ConsoleColor.Green;
+                FileInfo file = new FileInfo(json);
+                Console.WriteLine("\n[+] Swapped " + file.FullName);
+            }
+            Console.WriteLine("Memory has been released and the dynamical swapper can be closed now.");
+            Console.ReadLine();
+
+        }
+
+        public static string GetResult(string fileStr, string str)
+        {
+            dynamic jsonFile = JsonConvert.DeserializeObject(File.ReadAllText(fileStr));
+
+            return $"{jsonFile[str]}";
+        }
+    }
+}
